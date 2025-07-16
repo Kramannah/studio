4bba5a3d134492d30b7f643918dc8cd4130133d9
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2025-07-16 08:03:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/master-list.tsx
+++ src/components/master-list.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import * as XLSX from 'xlsx';
 import { useToast } from "@/hooks/use-toast";
