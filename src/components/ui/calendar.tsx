9d752d2e24d29f665ceedeff31d07b380e035062
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2025-07-29 07:04:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -37,7 +37,7 @@ function Calendar({
         head_cell:
           "text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-20 w-full text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-20 w-full text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-20 w-full p-0 font-normal aria-selected:opacity-100"
@@ -58,7 +58,7 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
         Day: ({ date, displayMonth, activeModifiers, ...props }: DayProps) => {
-          const { nonCall, weekend } = activeModifiers;
+          const { nonCall, weekend } = activeModifiers || {};
           if (nonCall) {
             return (
               <div
