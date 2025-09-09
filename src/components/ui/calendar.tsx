3c70a6f0ae4cd4e7e464b82b8e20b7004f5eff0b
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2025-09-09 08:33:36 +0000

    please make it bigger and easy to select (_for element <Root>_)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -37,10 +37,10 @@ function Calendar({
         head_cell:
           "text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-20 w-full text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-24 w-full text-center text-base p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-20 w-full p-0 font-normal aria-selected:opacity-100"
+          "h-24 w-full p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_end: "day-range-end",
         day_selected:
@@ -65,7 +65,7 @@ function Calendar({
                 {...props}
                 className={cn(
                   buttonVariants({ variant: "ghost" }),
-                  "h-20 w-full p-0 font-normal relative"
+                  "h-24 w-full p-0 font-normal relative"
                 )}
               >
                 <div className="absolute inset-0 bg-yellow-400/20"></div>
@@ -77,7 +77,7 @@ function Calendar({
           }
           if(weekend) {
             return (
-              <div {...props} className={cn(buttonVariants({ variant: "ghost" }), "h-20 w-full p-0 font-normal relative")}>
+              <div {...props} className={cn(buttonVariants({ variant: "ghost" }), "h-24 w-full p-0 font-normal relative")}>
                  <div className="absolute inset-0 bg-yellow-400/20"></div>
                 <span className="relative z-10">{date.getDate()}</span>
               </div>
