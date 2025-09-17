e2fa7958ef93f443d829bc1ba82e125caecb8d6a
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2025-09-17 07:02:20 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,43 +3,12 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DayProps, useDayRender } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
-
-const CustomDay = (props: DayProps) => {
-    const { buttonRef, activeModifiers, ...dayRender } = useDayRender(
-        props.date,
-        props.displayMonth,
-        props.buttonProps
-    );
-
-    const { nonCall, weekend } = activeModifiers;
-
-    if (dayRender.isHidden) {
-        return <></>
-    }
-
-    return (
-        <button
-            {...dayRender.buttonProps}
-            ref={buttonRef}
-            className={cn(
-                dayRender.buttonProps.className,
-                "h-full w-full p-0 font-normal",
-                weekend && 'bg-yellow-400/20',
-                nonCall && 'bg-yellow-400/20 text-red-500 line-through'
-            )}
-        >
-            <div className="flex items-center justify-center h-full w-full">
-                {dayRender.formattedDate}
-            </div>
-        </button>
-    )
-}
 
 function Calendar({
   className,
@@ -53,7 +22,7 @@ function Calendar({
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4 w-full",
+        month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-lg font-medium",
         nav: "space-x-1 flex items-center",
@@ -64,21 +33,21 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex w-full",
+        head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-24 w-full text-center text-base p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-24 w-full p-0 font-normal aria-selected:opacity-100"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
-          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
@@ -88,7 +57,6 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-        Day: CustomDay,
       }}
       {...props}
     />
