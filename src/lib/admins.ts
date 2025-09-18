a7689f9f1bb902b81a4363a0d4eacfcdf113fda4
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2025-09-18 03:00:05 +0000

    Try fixing this error: `Runtime Error: Error: SgOR5cjCC6dZ0oABv4nXdntu6p

--- src/lib/admins.ts
+++ src/lib/admins.ts
@@ -1,7 +1,4 @@
 // This is a temporary list of admin UIDs.
 // In a production environment, you should manage roles in a more robust way,
 // for example, using a 'roles' collection in Firestore or custom claims.
-export const ADMIN_UIDS = [
-  // Add the Firebase UID of admin users here.
-  // Example: "wXYZAbcdeFGHijKLMnOPq1r2s3t4",
-];
+export const ADMIN_UIDS = ["SgOR5cjCC6dZ0oABv4nXdntu6pI3"];
