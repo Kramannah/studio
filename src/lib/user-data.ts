a1875e8f7faa81ef5f70ec49a152387ed805d62c
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2025-09-30 02:31:19 +0000

    here's the list of user link to UID, please implement that code only vis

--- src/lib/user-data.ts
+++ src/lib/user-data.ts
@@ -1,9 +1,88 @@
 
 export const USER_DATA_MAP: Record<string, { code: string; firstName: string; lastName: string }> = {
-    "JvBQZydSDxNkZRP2vNofGXMGGk1": {
+    "JvBQZydSDxNkZRP2vNofGXMGbGk1": {
         code: "NL-04",
         firstName: "Maribel",
         lastName: "Aguinaldo",
     },
-    // Add other user mappings here
+    "JvY6C2uEdPeEqoWLuqB4eYfbZqj2": {
+        code: "NL-05",
+        firstName: "Janet",
+        lastName: "Tagao",
+    },
+    "mZJZjTMVinNRegZFQG9FZzRjpiA2": {
+        code: "CL-02",
+        firstName: "Marlhan Lloyd P.",
+        lastName: "De Guia",
+    },
+    "n7J3AJy6KIXQYMEil0TyQQcAxs93": {
+        code: "CL-03",
+        firstName: "Odessa",
+        lastName: "Singian",
+    },
+    "xNTJrZ5xXwRliGvpCVXaxDwDpl32": {
+        code: "CL-04",
+        firstName: "Arnel Jr",
+        lastName: "Bongala",
+    },
+    "8QiLTKzhovh43RX2arJ4xNAgoRj2": {
+        code: "MQ-02",
+        firstName: "John Martin G.",
+        lastName: "Ponce",
+    },
+    "mUP19b2ISPc4Qnl1e0MHbCKAAKH3": {
+        code: "MQ-04",
+        firstName: "Cynthia May",
+        lastName: "Cain",
+    },
+    "e4U1TKIhzURRt3YdqKWQgkDQApv2": {
+        code: "MQ-05",
+        firstName: "Julius",
+        lastName: "Jalandoni",
+    },
+    "cm4yqA8NfBadUBtffEzcEJRV9873": {
+        code: "GMAS-04",
+        firstName: "Mikka Amielle",
+        lastName: "Clement",
+    },
+    "pvzNDFxH4tf7JejE7YxMK4Rq3hj2": {
+        code: "GMAS-02",
+        firstName: "Lylle",
+        lastName: "Caguimbal",
+    },
+    "l5GMEE0OxpMOObd5qguyJtrVUi42": {
+        code: "GMAS-03",
+        firstName: "Jacquelyn",
+        lastName: "Dreu",
+    },
+    "ledPtMmuniSwpkYZ5YAXOSREoxl2": {
+        code: "GMAS-05",
+        firstName: "Ellen",
+        lastName: "Baluyot",
+    },
+    "sN4SztOibRSTg1H7SEeAwiEpDgm1": {
+        code: "GMAS-07",
+        firstName: "Michael V",
+        lastName: "Cruz",
+    },
+    "uDhb49uS55XJm8cp6bqL2adjfwh2": {
+        code: "MQ-06",
+        firstName: "Jesus Alain",
+        lastName: "Manalo",
+    },
+    "AhkYw50sBueJFvRO8glPlpE4QWt2": {
+        code: "MIN-01",
+        firstName: "Ian Cris",
+        lastName: "Natinga",
+    },
+    "wHns6CCKRde4YMf8VZhxLJfAa6H3": {
+        code: "MIN-02",
+        firstName: "Emelyn Grace D",
+        lastName: "Dolores",
+    },
+    "BjsmUvmlFdUMDKg123iibi5AxZA2": {
+        code: "LSL-06",
+        firstName: "Richelle M.",
+        lastName: "Astor",
+    }
 };
