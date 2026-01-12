8af40aa4ac821e2dabc937d87b563824049a52d6
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2026-01-12 00:45:52 +0000

    please change the code to able to publish

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    TZ: 'Asia/Manila',
+  },
+};
+
+module.exports = withPWA(nextConfig);
