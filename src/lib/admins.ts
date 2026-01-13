0b67c17ee064a14c14387e07b8d47fe54a20ff2c
Mark Michael Bustamante <markmichaelbustamante23@gmail.com>
2026-01-13 07:26:02 +0000

    please  make this log in as Admin to view all reports of user's m2ZTNUi5

--- src/lib/admins.ts
+++ src/lib/admins.ts
@@ -2,7 +2,7 @@
 // This is a temporary list of admin UIDs.
 // In a production environment, you should manage roles in a more robust way,
 // for example, using a 'roles' collection in Firestore or custom claims.
-export const ADMIN_UIDS = ["SgOR5cjCC6dZ0oABv4nXdntu6pI3"];
+export const ADMIN_UIDS = ["SgOR5cjCC6dZ0oABv4nXdntu6pI3", "m2ZTNUi5v9ef82FxVRbwSmyGv9S2"];
 
 
 // Defines which users a manager can see in their admin dashboard.
