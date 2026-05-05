e056b7b6edc5778d909d8ccc8816e5b2c6cb39e2
Saroo <angelbenedictreveche@gmail.com>
2026-05-05 02:11:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "hovidcoverage",
+  "appId": "1:321369261510:web:9c4b76565d68b8c9875ec7",
+  "apiKey": "AIzaSyCqtWu5SXQmQllqN61DM80uquFa0K06QCE",
+  "authDomain": "hovidcoverage.firebaseapp.com",
+  "measurementId": "G-29Y9Y3BM5N",
+  "messagingSenderId": "321369261510"
+};
