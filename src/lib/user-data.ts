eb8475349d2fdfdf6f65810149bdded82a329b63
Saroo <angelbenedictreveche@gmail.com>
2026-05-06 06:57:47 +0000

    Not like that I meant the email value under the Identifier in Authentica

--- src/lib/user-data.ts
+++ src/lib/user-data.ts
@@ -4,109 +4,109 @@ export const USER_DATA_MAP: Record<string, { code: string; firstName: string; la
         code: "NL-04",
         firstName: "Maribel",
         lastName: "Aguinaldo",
-        email: "maribel.aguinaldo@hovidinc.com"
+        email: "maguinaldo@hovidinc.com"
     },
     "JvY6C2uEdPeEqoWLuqB4eYfbZqj2": {
         code: "NL-05",
         firstName: "Janet",
         lastName: "Tagao",
-        email: "janet.tagao@hovidinc.com"
+        email: "jtagao@hovidinc.com"
     },
     "mZJZjTMVinNRegZFQG9FZzRjpiA2": {
         code: "CL-02",
         firstName: "Marlhan Lloyd P.",
         lastName: "De Guia",
-        email: "marlhan.deguia@hovidinc.com"
+        email: "mldeguia@hovidinc.com"
     },
     "n7J3AJy6KIXQYMEil0TyQQcAxs93": {
         code: "CL-03",
         firstName: "Odessa",
         lastName: "Singian",
-        email: "odessa.singian@hovidinc.com"
+        email: "osingian@hovidinc.com"
     },
     "xNTJrZ5xXwRliGvpCVXaxDwDpl32": {
         code: "CL-04",
         firstName: "Arnel Jr",
         lastName: "Bongala",
-        email: "arnel.bongala@hovidinc.com"
+        email: "abongala@hovidinc.com"
     },
     "8QiLTKzhovh43RX2arJ4xNAgoRj2": {
         code: "MQ-02",
         firstName: "John Martin G.",
         lastName: "Ponce",
-        email: "john.ponce@hovidinc.com"
+        email: "jmgponce@hovidinc.com"
     },
     "mUP19b2ISPc4Qnl1e0MHbCkAAKH3": {
         code: "MQ-04",
         firstName: "Cynthia May",
         lastName: "Cain",
-        email: "cynthia.cain@hovidinc.com"
+        email: "cmcain@hovidinc.com"
     },
     "e4U1TKIhzURRt3YdqKWQgkDQApv2": {
         code: "MQ-05",
         firstName: "Julius",
         lastName: "Jalandoni",
-        email: "julius.jalandoni@hovidinc.com"
+        email: "jjalandoni@hovidinc.com"
     },
     "cm4yqA8NfBadUBtffEzcEJRV9873": {
         code: "GMAS-04",
         firstName: "Mikka Amielle",
         lastName: "Clement",
-        email: "mikka.clement@hovidinc.com"
+        email: "maclement@hovidinc.com"
     },
     "pvzNDFxH4tf7JejE7YxMK4Rq3hj2": {
         code: "GMAS-02",
         firstName: "Lylle",
         lastName: "Caguimbal",
-        email: "lylle.caguimbal@hovidinc.com"
+        email: "lcaguimbal@hovidinc.com"
     },
     "l5GMEE0OxpMOObd5qguyJtrVUi42": {
         code: "GMAS-03",
         firstName: "Jacquelyn",
         lastName: "Dreu",
-        email: "jacquelyn.dreu@hovidinc.com"
+        email: "jdreu@hovidinc.com"
     },
     "ledPtMmuniSwpkYZ5YAXOSREoxl2": {
         code: "GMAS-05",
         firstName: "Ellen",
         lastName: "Baluyot",
-        email: "ellen.baluyot@hovidinc.com"
+        email: "ebaluyot@hovidinc.com"
     },
     "sN4SztOibRSTg1H7SEeAwiEpDgm1": {
         code: "GMAS-07",
         firstName: "Michael V",
         lastName: "Cruz",
-        email: "michael.cruz@hovidinc.com"
+        email: "mvcruz@hovidinc.com"
     },
     "uDhb49uS55XJm8cp6bqL2adjfwh2": {
         code: "MQ-06",
         firstName: "Jesus Alain",
         lastName: "Manalo",
-        email: "jesus.manalo@hovidinc.com"
+        email: "jamanalo@hovidinc.com"
     },
     "AhkYw50sBueJFvRO8glPlpE4QWt2": {
         code: "MIN-01",
         firstName: "Ian Cris",
         lastName: "Natinga",
-        email: "ian.natinga@hovidinc.com"
+        email: "icnatinga@hovidinc.com"
     },
     "wHns6CCKRde4YMf8VZhxLJfAa6H3": {
         code: "MIN-02",
         firstName: "Emelyn Grace D",
         lastName: "Dolores",
-        email: "emelyn.dolores@hovidinc.com"
+        email: "egdolores@hovidinc.com"
     },
     "BjsmUvmlFdUMDKg123iibi5AxZA2": {
         code: "LSL-06",
         firstName: "Richelle M.",
         lastName: "Astor",
-        email: "richelle.astor@hovidinc.com"
+        email: "rmastor@hovidinc.com"
     },
     "UmyUP0VwccRKLfrej67idFukFz42": {
         code: "DSM",
         firstName: "Azarel",
         lastName: "Daquioag",
-        email: "azarel.daquioag@hovidinc.com"
+        email: "adaquioag@hovidinc.com"
     },
     "SgOR5cjCC6dZ0oABv4nXdntu6pI3": {
         code: "ADMIN",
@@ -118,211 +118,211 @@ export const USER_DATA_MAP: Record<string, { code: string; firstName: string; la
         code: "DSM",
         firstName: "Erica",
         lastName: "Gonzales",
-        email: "erica.gonzales@hovidinc.com"
+        email: "egonzales@hovidinc.com"
     },
     "I3HkxWKsKZOPdmUT9Hi1G8JUy5t1": {
         code: "DSM",
         firstName: "Eula",
         lastName: "Candawan",
-        email: "eula.candawan@hovidinc.com"
+        email: "ecandawan@hovidinc.com"
     },
     "e11qs3XD1vW3JWHfEaJI2DXcCEj2": {
         code: "DSM",
         firstName: "Marisol",
         lastName: "Nonato",
-        email: "marisol.nonato@hovidinc.com"
+        email: "mnonato@hovidinc.com"
     },
     "XNfz4EiMrIRQ4NwLlkdUXUvBzSH2": {
         code: "NL-01",
         firstName: "John Patrick",
         lastName: "Padilla",
-        email: "john.padilla@hovidinc.com"
+        email: "jppadilla@hovidinc.com"
     },
     "mdLCjhNVnYas96aW4IkrPWip7RS2": {
         code: "NL-02",
         firstName: "Khristopher George",
         lastName: "Pangan",
-        email: "khristopher.pangan@hovidinc.com"
+        email: "kgpangan@hovidinc.com"
     },
     "ePVOaPvZYTa9CGj0MpReVnmKhD62": {
         code: "NL-03",
         firstName: "Cheysa",
         lastName: "Catama",
-        email: "cheysa.catama@hovidinc.com"
+        email: "ccatama@hovidinc.com"
     },
     "Tajceo3bwwcH9ac9Mw4tEtj2Z952": {
         code: "CL-01",
         firstName: "Pauline Jean T.",
         lastName: "Ramos",
-        email: "pauline.ramos@hovidinc.com"
+        email: "pjramos@hovidinc.com"
     },
     "nZZHI6JVZzZCVBIDcwiAv0RDmGv2": {
         code: "CL-05",
         firstName: "Carmina",
         lastName: "Dimaala",
-        email: "carmina.dimaala@hovidinc.com"
+        email: "cdimaala@hovidinc.com"
     },
     "NOYZ3h8ylaNINBVBgFYJzhKdisY2": {
         code: "MQ-01",
         firstName: "Luzviminda",
         lastName: "Rivera",
-        email: "luzviminda.rivera@hovidinc.com"
+        email: "lrivera@hovidinc.com"
     },
     "sRqr0SXKIUZBCn5g9V6VQRh24SS2": {
         code: "LSL-01",
         firstName: "Joida",
         lastName: "Meniosa",
-        email: "joida.meniosa@hovidinc.com"
+        email: "jmeniosa@hovidinc.com"
     },
     "xOQdg23cnOgLQD5lonJtFgAELa13": {
         code: "LSL-03",
         firstName: "Marylyn",
         lastName: "Baldoz",
-        email: "marylyn.baldoz@hovidinc.com"
+        email: "mbaldoz@hovidinc.com"
     },
     "Hnr2ehdTK0O8PfSj7Vn2zQksqz63": {
         code: "LSL-04",
         firstName: "Sheena Marie",
         lastName: "Callejo",
-        email: "sheena.callejo@hovidinc.com"
+        email: "smcallejo@hovidinc.com"
     },
     "h2q00DOa3EM870V5RDu2NABcb213": {
         code: "LSL-05",
         firstName: "Carmel",
         lastName: "Tojon",
-        email: "carmel.tojon@hovidinc.com"
+        email: "ctojon@hovidinc.com"
     },
     "2Bjo0AEdTbWb14C4knsPvX0Ub8r1": {
         code: "GMAS-01",
         firstName: "Iries",
         lastName: "Rombaiz",
-        email: "iries.rombaiz@hovidinc.com"
+        email: "irombaiz@hovidinc.com"
     },
     "MkQwPWR0wyXrDRzlekEFMzQSu7Y2": {
         code: "GMAS-06",
         firstName: "Gwyne Joseph F.",
         lastName: "Lestor",
-        email: "gwyne.lestor@hovidinc.com"
+        email: "gjlestor@hovidinc.com"
     },
     "WZkgG4Ot9jRssWT3E09XIwe6VFc2": {
         code: "ONCO",
         firstName: "Aileen",
         lastName: "Nañas",
-        email: "aileen.nanas@hovidinc.com"
+        email: "ananas@hovidinc.com"
     },
     "hM21Pxjwfma3m75pefiM7j23F4f1": {
         code: "VIS-01",
         firstName: "Eliazar",
         lastName: "Pagobo",
-        email: "eliazar.pagobo@hovidinc.com"
+        email: "epagobo@hovidinc.com"
     },
     "px4HMkypSRMTuQFrY21bFEJoYA73": {
         code: "VIS-02",
         firstName: "Anne",
         lastName: "Alberto",
-        email: "anne.alberto@hovidinc.com"
+        email: "aalberto@hovidinc.com"
     },
     "5JJythHRfYT8PllnH7kcWoHDSIO2": {
         code: "VIS-04",
         firstName: "Alvin",
         lastName: "Masana",
-        email: "alvin.masana@hovidinc.com"
+        email: "amasana@hovidinc.com"
     },
     "CrwnET2Cohhqf4lsYiI8AYPtlPo1": {
         code: "VIS-05",
         firstName: "Armadilla",
         lastName: "Sarahina",
-        email: "armadilla.sarahina@hovidinc.com"
+        email: "asarahina@hovidinc.com"
     },
     "mxcI6Z9EjKWYmvNisUCIzSPi4573": {
         code: "VIS-08",
         firstName: "Joenilo",
         lastName: "Torpez",
-        email: "joenilo.torpez@hovidinc.com"
+        email: "jtorpez@hovidinc.com"
     },
     "qsFiDEpOgOcTFSbiX2RJMrCKBHG3": {
         code: "MQ-03",
         firstName: "Mark Anthony D.",
         lastName: "Capilos",
-        email: "mark.capilos@hovidinc.com"
+        email: "macapilos@hovidinc.com"
     },
     "H5NGDRDneWdH9ADuZDCFNHlovK83": {
         code: "VIS-06",
         firstName: "Edcel B",
         lastName: "Babas",
-        email: "edcel.babas@hovidinc.com"
+        email: "ebabas@hovidinc.com"
     },
     "t8WMcGOCaQdB159ZeNBmHZvEtR13": {
         code: "VIS-07",
         firstName: "Charles V.",
         lastName: "Salomeo",
-        email: "charles.salomeo@hovidinc.com"
+        email: "cvsalomeo@hovidinc.com"
     },
     "x8u4kvWvieZIVc9NdgiWmG01nts2": {
         code: "DSM_MIN",
         firstName: "Rex",
         lastName: "Ligutom",
-        email: "rex.ligutom@hovidinc.com"
+        email: "rligutom@hovidinc.com"
     },
     "Dr2aymosXAUZP4PPagrNSG7UTFg1": {
         code: "MIN-03",
         firstName: "Kaye Angela",
         lastName: "Daquipil",
-        email: "kaye.daquipil@hovidinc.com"
+        email: "kadaquipil@hovidinc.com"
     },
     "XSQqh3BUr7Nex40R9PgjLjWfVVF2": {
         code: "MIN-04",
         firstName: "Cyril",
         lastName: "Calzada",
-        email: "cyril.calzada@hovidinc.com"
+        email: "ccalzada@hovidinc.com"
     },
     "iBzfeR7QujgmyUT2ogY0cN3KrLC3": {
         code: "MIN-05",
         firstName: "Geonalin",
         lastName: "Naraval",
-        email: "geonalin.naraval@hovidinc.com"
+        email: "gnaraval@hovidinc.com"
     },
     "8qS5baPi4YRwX1sa4g2m1ZMEDfX2": {
         code: "MIN-06",
         firstName: "Liezel",
         lastName: "Lacap",
-        email: "liezel.lacap@hovidinc.com"
+        email: "llacap@hovidinc.com"
     },
     "bu35X2zJWmckJXGLt0yU3G6G54E3": {
         code: "MIN-07",
         firstName: "Rosemarie",
         lastName: "Espiritu",
-        email: "rosemarie.espiritu@hovidinc.com"
+        email: "respiritu@hovidinc.com"
     },
     "w90JOQlrOJOf2FVq0iU2krdMQzI3": {
         code: "MIN-08",
         firstName: "Mark John",
         lastName: "Bartania",
-        email: "mark.bartania@hovidinc.com"
+        email: "mjbartania@hovidinc.com"
     },
     "SuPo0lvdIZPAgZb6zYrnlfPNchl1": {
         code: "MIN-09",
         firstName: "Rizza Mae",
         lastName: "Quinto",
-        email: "rizza.quinto@hovidinc.com"
+        email: "rmquinto@hovidinc.com"
     },
     "MlE49ceLEDbzvKAsisKnW07fGfW2": {
         code: "LSL-DSM",
         firstName: "Carmen Joy",
         lastName: "Cabangon",
-        email: "carmen.cabangon@hovidinc.com"
+        email: "cjcabangon@hovidinc.com"
     },
     "r19lVIqk4xTtzxvbs4W0kprHSvD2": {
         code: "DSM_VIS",
         firstName: "Joey",
         lastName: "Ignacio",
-        email: "joey.ignacio@hovidinc.com"
+        email: "jignacio@hovidinc.com"
     },
     "lDprIWp1acWrMElyzk2uTjQ4q4m1": {
         code: "DSM",
         firstName: "Ramonalyn Jake",
         lastName: "Garcia",
-        email: "ramonalyn.garcia@hovidinc.com"
+        email: "rjgarcia@hovidinc.com"
     },
     "nBNzGCEnXuMKXOCeZGG3P6k6Spm1": {
         code: "VIS-NEW",
