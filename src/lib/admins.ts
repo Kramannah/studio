48fd7871441629730d20668aa59c236770f6b098
Saroo <angelbenedictreveche@gmail.com>
2026-06-19 00:45:48 +0000

    Under Azarel Daquioag add this PMR representative with this UID AhkYw50s

--- src/lib/admins.ts
+++ src/lib/admins.ts
@@ -25,7 +25,8 @@ export const MANAGER_TEAMS: Record<string, string[]> = {
         "l5GMEE0OxpMOObd5qguyJtrVUi42",
         "IedPtMmuniSwpkYZ5YAXOSREoxl2",
         "fwBf7XGUb6MSJygdL5NitBcbHRA3",
-        "WkLHNPDvLmfJBvQSDSn5ImY3nkg1"
+        "WkLHNPDvLmfJBvQSDSn5ImY3nkg1",
+        "AhkYw50sBueJFvRO8glPlpE4QWt2"
     ],
   "tb73WEIzndUrPEaD1q4H0VQfQaD3": [
         "8QiLTKzhovh43RX2arJ4xNAgoRj2",
